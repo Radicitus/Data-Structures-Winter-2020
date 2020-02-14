f548e2ff59e5aa2dfa93de32170b5a4e8f3c9c3f
Radicitus@Lupus <crsherry@uci.edu>
2020-02-14 01:13:02 -0800

    Completed Project3

--- Project3/proj3.cpp
+++ Project3/proj3.cpp
@@ -1,11 +1,10 @@
 #include "proj3.hpp"
 #include "Wordset.hpp"
 #include <iostream>
-#include <set>
 #include <sstream>
+#include <queue>
 #include <map>
 #include <stack>
-#include <queue>
 
 // You should not need to change this function.
 void loadWordsIntoTable(WordSet & words, std::istream & in)
@@ -26,9 +25,74 @@ void loadWordsIntoTable(WordSet & words, std::istream & in)
 }
 
 
+std::stringstream toString(const std::string& s1, const std::string& s2, std::map<std::string, std::string> & trail) {
+    std::stringstream stringTrail;
+    std::stack<std::string> flip;
+
+    std::string key = s2;
+
+    while (key != s1 && !trail.empty()) {
+        if (key != s2) {flip.push(key);}
+        key = trail.at(key);
+       
+    }
+
+
+    stringTrail << s1 << " --> ";
+    int flipSize = flip.size();
+    for (int i = 0; i < flipSize; ++i) {
+        stringTrail << flip.top() << " --> ";
+        flip.pop();
+    }
+
+    stringTrail << s2;
+
+    return stringTrail;
+}
+
 // You probably want to change this function.
 std::string convert(std::string s1, std::string s2, const WordSet & words)
 {
+    std::map <std::string, std::string> stringTrail;
+    std::queue <std::string> breadthSearch;
+    breadthSearch.push(s1);
 
-	return "foo";  // stub obviously 
+    //if (!words.contains(s2)) {return s1;}
+
+    while (!breadthSearch.empty()) {
+
+        int currentSize = breadthSearch.size();
+
+        for (int i = 0; i < currentSize; ++i) {
+            std::string currentWord = breadthSearch.front();
+            breadthSearch.pop();
+
+            for (int j = 0; j < s1.size(); ++j) {
+
+                std::string rootString = currentWord;
+                char rootChar = currentWord[j];
+
+                for (char c = 'a'; c <= 'z'; ++c) {
+
+                    currentWord[j] = c;
+
+                    if (currentWord == s2) {
+                    	stringTrail.insert( {s2, rootString} );
+                        return toString(s1, s2, stringTrail).str();
+                    }
+
+                    if (words.contains(currentWord) && (stringTrail.find(currentWord) == stringTrail.end())) {
+                        stringTrail.insert( {currentWord, rootString} );
+                        breadthSearch.push(currentWord);
+                    }
+                }
+
+                currentWord[j] = rootChar;
+
+            }
+        }
+    }
+
+    return toString(s1, s2, stringTrail).str();
+
 }
